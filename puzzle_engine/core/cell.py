807535e0a75c19573f13c97e7cf55aa500f3a358
David Nelson <thedoorman27@yahoo.com>
2026-01-16 00:14:26 -0700

    Basic Cell Logic initial

---
+++ puzzle_engine/core/cell.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from .enums import ParityState, CellOrigin
+
+
+class Cell:
+    def __init__(self, row: int, col: int):
+        self.row = row
+        self.col = col
+
+        self.value: Optional[int] = None # sudoku stuff
+        self.origin: Optional[CellOrigin] = None
+
+        self.parity_state: ParityState = ParityState.UNKNOWN # picross stuff
+
+    def set_value(self, value: int, origin: CellOrigin):
+        if self.origin == CellOrigin.GIVEN:
+            raise ValueError(f"Cannot change a given cell at ({self.row},{self.col})")
+        
+        if not 1 <= value <= 9:
+            raise ValueError("Sudoku values must be between 1 and 9")
+
+        self.value = value
+        self.origin = origin
+
+    def set_parity_state(self, state: ParityState): # picross marking
+        self.parity_state = state
+
+    def clear_value(self):
+        if self.origin == CellOrigin.GIVEN:
+            raise ValueError("Cannot clear a given cell")
+
+        self.value = None
+        self.origin = None
+        self.parity_state = ParityState.UNKNOWN
+
+    def is_empty(self) -> bool:
+        return self.value is None
---
+++ puzzle_engine/core/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum, auto
+
+class ParityState(Enum): # picross states
+    FILLED = auto()  # paired with even sudoku numbers
+    EMPTY = auto()   # paired with odd sudoku numbers
+    UNKNOWN = auto() # not yet filled
+
+
+class CellOrigin(Enum): # so GUI knows what can/can't be edited
+    GIVEN = auto()      # should be uneditable
+    USER = auto()
